ddab6de95b2d96084a09cc660ef42550c1daaa79
Raghad-barakat <r.a.barakat@students.ptuk.edu.ps>
2026-08-31 22:09:25 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,9 @@
 window.WORKTRACK_CONFIG = {
-  SUPABASE_URL: "https://jdysqmhaomivegvqlgwt.supabase.co",
-  SUPABASE_KEY: "sb_publishable_TMJ7TXC3yA6RvgF9x2lI1Q_7KMAPN3c"
+
+  SUPABASE_URL:
+    "https://jdysqmhaomivegvqlgwt.supabase.co",
+
+  SUPABASE_KEY:
+    "sb_publishable_TMJ7TXC3yA6RvgF9x2lI1Q_7KMAPN3c"
+
 };
